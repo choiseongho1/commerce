d62d8b16a56947bedc262bd3a5d514ea0d63238d
choiseongho <choiseongho@unfine.co.kr>
2024-06-28 14:22:50 +0900

    20240628

--- src/main/java/com/ho/commerce/api/optionvalue/domain/OptionValue.java
+++ src/main/java/com/ho/commerce/api/optionvalue/domain/OptionValue.java
@@ -3,10 +3,12 @@ package com.ho.commerce.api.optionvalue.domain;
 import com.ho.commerce.api.productoption.domain.ProductOption;
 import jakarta.persistence.*;
 
+@Table(name = "OPTION_VALUE")
 @Entity
 public class OptionValue {
 
     @Id
+    @Column(name = "OPTION_VALUE_ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long optionValueId;
 
