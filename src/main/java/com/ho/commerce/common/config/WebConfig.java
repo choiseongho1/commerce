c0c2ef75414b1cb6ef5bfe5cf4922f07eeeea568
choiseongho <choiseongho@unfine.co.kr>
2024-07-08 10:10:03 +0900

    20240708 webconfig 원복

--- src/main/java/com/ho/commerce/common/config/WebConfig.java
+++ src/main/java/com/ho/commerce/common/config/WebConfig.java
@@ -1,19 +1,41 @@
 package com.ho.commerce.common.config;
 
-
+import com.ho.commerce.common.interceptor.BaseDtoInterceptor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @Configuration
+@Slf4j
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private BaseDtoInterceptor baseDtoInterceptor;
+
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
         registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000", "http://43.202.204.185")
                 .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(baseDtoInterceptor)
+                .addPathPatterns("/**"); // 모든 경로에 대해 인터셉터 적용
     }
 }
