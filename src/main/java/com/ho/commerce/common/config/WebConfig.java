11c7446965744d3402959b1e9df920e8c8fbc4da
choiseongho <choiseongho@unfine.co.kr>
2024-07-08 11:31:57 +0900

    20240708 WebConfig.java 추가

--- src/main/java/com/ho/commerce/common/config/WebConfig.java
+++ src/main/java/com/ho/commerce/common/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.ho.commerce.common.interceptor.BaseDtoInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,6 +14,16 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     private BaseDtoInterceptor baseDtoInterceptor;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
